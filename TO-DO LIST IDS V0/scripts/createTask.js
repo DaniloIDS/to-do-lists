330e0e3baa56977d2dc8029215b56050eedd677b
DaniloIDS <danilo.ids@outlook.com>
2022-08-02 21:40:16 -0300

    Ajustando o createTask

--- TO-DO LIST IDS V0/scripts/createTask.js
+++ TO-DO LIST IDS V0/scripts/createTask.js
@@ -6,14 +6,23 @@ function sendingTask(){
   const task = document.querySelector('#inputTask')
   const list = document.querySelector('.listTasks > ul')
   
-
   const item = document.createElement('li')
+  const removeButton = document.createElement('button')
   const contentItem = document.createElement('p')
-  contentItem.setAttribute('class', 'task')
-  
+  const div = document.createElement('div')
+  const editButton = document.createElement('button')
+  const checkButton = document.createElement('button')
+
+  item.setAttribute('class', 'taskContainer')
+  contentItem.setAttribute('class', 'taskValue')
+  removeButton.setAttribute('class', 'removeButton')
+  editButton.setAttribute('class', 'editButton')
+  checkButton.setAttribute('class', 'checkButton')
+
   contentItem.innerText = task.value
 
-  item.appendChild(contentItem)
+  div.append(editButton, checkButton)
+  item.append(removeButton, contentItem, div)
 
   list.appendChild(item)
 

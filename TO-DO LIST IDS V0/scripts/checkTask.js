123cea69abbf0e1f1a4c158b9467109295f7389f
DaniloIDS <danilo.ids@outlook.com>
2022-08-04 21:18:23 -0300

    Correção bug edit-check

--- TO-DO LIST IDS V0/scripts/checkTask.js
+++ TO-DO LIST IDS V0/scripts/checkTask.js
@@ -8,9 +8,12 @@ function callCheckTask(){
 
 function checkTask(element){
   let task = element.composedPath()[2]
+ 
   task.children[2].children[0].removeEventListener('click', editTask)
+  task.children[1].style.border = "none"
   task.children[1].style.textDecoration = "line-through"
   task.style.backgroundColor = "lightgreen"
   task.children[1].style.backgroundColor = "lightgreen"
+  task.children[1].disabled = true
   
 }
